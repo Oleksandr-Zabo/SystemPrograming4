9237999633aa0434fdbdeab4ce7cb58f5a8375f5
Oleksandr-Zabo <zoleksandr06@gmail.com>
2025-04-15 19:21:30 +0300

    ex. 3 had done

--- SystemPrograming4/Program.cs
+++ SystemPrograming4/Program.cs
@@ -1,32 +1,62 @@
 ﻿using System;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace SystemPrograming4;
-
-class Program
+namespace SystemPrograming4
 {
-    static async Task Main(string[] args)
+    class Program
     {
-        Console.Write("Enter start of range: ");
-        int startRange = Convert.ToInt32(Console.ReadLine());
-        
-        Console.Write("Enter end of range: ");
-        int endRange = Convert.ToInt32(Console.ReadLine());
-        
-        if(startRange> endRange)
+        private static object _lockObject = new object();
+        private static int _current = 0;
+
+        static void Main(string[] args)
         {
-            (startRange, endRange) = (endRange, startRange);
-        }
-        
-        Console.WriteLine("Starting thread...");
-        await Task.Run(() =>
-        {
+            Console.Write("Enter start of range: ");
+            int startRange = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter end of range: ");
+            int endRange = Convert.ToInt32(Console.ReadLine());
+
+            if (startRange > endRange)
+            {
+                (startRange, endRange) = (endRange, startRange);
+            }
+
+            Console.WriteLine("Starting threads...");
+
+            int rangeCount = endRange - startRange + 1;
+            Thread[] threads = new Thread[rangeCount];
+
             for (int i = startRange; i <= endRange; i++)
             {
-                Console.WriteLine(i);
+                int number = i; // Capture the current value of i
+                threads[i - startRange] = new Thread(() => PrintNumberSequentially(number));
+                threads[i - startRange].Start();
             }
-        });
-        Console.WriteLine("Thread completed.");
+
+            // Wait for all threads to complete
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            Console.WriteLine("All threads completed.");
+        }
+
+        static void PrintNumberSequentially(int number)
+        {
+            lock (_lockObject)
+            {
+                // Ensure numbers are printed in the correct sequence
+                while (_current != number - 1)
+                {
+                    Monitor.Wait(_lockObject);
+                }
+
+                // Print thread name (ID) and the number
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} processed number: {number}");
+                _current = number;
+                Monitor.PulseAll(_lockObject);
+            }
+        }
     }
 }
-
