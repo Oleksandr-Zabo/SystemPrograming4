5a05d1077acc60fd771296924e4aac15d391fb51
Oleksandr-Zabo <zoleksandr06@gmail.com>
2025-04-15 19:02:41 +0300

    ex. 1 had done

--- SystemPrograming4/Program.cs
+++ SystemPrograming4/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace SystemPrograming4;
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SystemPrograming4;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Starting thread...");
+        await Task.Run(() =>
+        {
+            for (int i = 0; i <= 50; i++)
+            {
+                Console.WriteLine(i);
+            }
+        });
+        Console.WriteLine("Thread completed.");
     }
 }
+
