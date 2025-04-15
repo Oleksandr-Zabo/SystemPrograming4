5f76b14caff1a29e2c12368549c403b10f49d019
Oleksandr-Zabo <zoleksandr06@gmail.com>
2025-04-15 19:10:35 +0300

    ex. 2 had done

--- SystemPrograming4/Program.cs
+++ SystemPrograming4/Program.cs
@@ -7,10 +7,21 @@ class Program
 {
     static async Task Main(string[] args)
     {
+        Console.Write("Enter start of range: ");
+        int startRange = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Enter end of range: ");
+        int endRange = Convert.ToInt32(Console.ReadLine());
+        
+        if(startRange> endRange)
+        {
+            (startRange, endRange) = (endRange, startRange);
+        }
+        
         Console.WriteLine("Starting thread...");
         await Task.Run(() =>
         {
-            for (int i = 0; i <= 50; i++)
+            for (int i = startRange; i <= endRange; i++)
             {
                 Console.WriteLine(i);
             }
